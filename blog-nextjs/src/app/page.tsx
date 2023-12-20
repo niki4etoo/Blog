ac5335bbbe0a1c52fca099516bf97212e82f2921
Nikolai Nanev <niki4etooo@gmail.com>
2023-12-20 09:48:00 +0200

    added component for recent blog posts

--- blog-nextjs/src/app/page.tsx
+++ blog-nextjs/src/app/page.tsx
@@ -1,8 +1,15 @@
+import RecentPosts from '@/components/RecentPosts'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>My Blog - Nikolai Nanev</h1>
+      <div className='text-center'>
+        <h2 className='font-bold'>Recent Posts</h2>
+        <RecentPosts />
+      </div>
+      
     </main>
   )
 }
